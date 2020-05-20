f371a3bfc983ce38973200399cadaea6a6525348
Thiago Rodrigues <thiagothogs@gmail.com>
2020-05-20 20:20:30 -0300

    Add code for appRegister

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-"""                      => README! <=
-*---------------------------------------------------------------------*
-*                                                                     *
-*    Requisitos:                                                      *
-*       => termcolor                                                  *
-*          - Windows: python3.8 pip install termcolor (cmd admin)     *
-*          - macOS/Linux: python3.8 pip install termcolor             *
-*                                                                     *
-*       => IDEs testada: PyCharm e VScode (VScode é recomendado)      *
-*          - para uma execusão limpa, exercutar diretamente no        *
-*            termnal (macOS/Linux) ou CMD (Windows)                   *
-*                                                                     *
-*       => Versão do Python testada 3.8.2                             *
-*                                                                     *
-*---------------------------------------------------------------------*
-"""
 
 import json
 import os
